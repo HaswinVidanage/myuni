df2ca252e77563d0469e5e187496fbb118da3f63
Haswin Vidanage <haswin@hbs.com>
2017-01-26 17:15:38 +0530

    Getting all relevant data to the views

--- routes/views/tickets/mycard.js
+++ routes/views/tickets/mycard.js
@@ -1,50 +1,52 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-	
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
-	
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'tickets';
-	
-    locals.data = { 
-		cards: [],
-	};
-    
+
+    var view = new keystone.View(req, res);
+    var locals = res.locals;
+
+    // locals.section is used to set the currently selected
+    // item in the header navigation.
+    locals.section = 'tickets';
+
+    locals.data = {
+        cards: [],
+    };
+
     // Load all tickets
-	view.on('init', function(next) {
+    view.on('init', function(next) {
 
-		// var q = keystone.list('Ticket').model.paginate({
-  //                   page: req.query.page || 1,
-  //                   perPage: 1,
-  //                   maxPages: 5
-  //               });
+        // var q = keystone.list('Ticket').model.paginate({
+        //                   page: req.query.page || 1,
+        //                   perPage: 1,
+        //                   maxPages: 5
+        //               });
 
-  	var q = keystone.list('Card').model.find({createdBy : res.locals.user}).populate('university institute title');		 
+        var q = keystone.list('Card').model.find({ createdBy: res.locals.user }).populate('university institute title');
 
-		q.exec(function(err, results) {
-			locals.data.cards = results;
+        q.exec(function(err, results) {
+            locals.data.cards = results;
 
-			//workaround to filter only needed data
-			results.forEach(function (results) {
-                            console.log("hello from the other side!!!");
-              });
-			//console.log("result : "+results);
-			console.log("university: "+ results[0].university.name); // Joe
-			console.log("institute: "+ results[0].institute.name); // Joe
-			console.log("course: "+ results[0].title.name); // Joe
- 		//  	console.log("university : "+results.populated('university'));  // '1234af9876b024c680d111a1' -> _id			
-			next(err);
-		});
+            //workaround to filter only needed data
+            // results.forEach(function (results) {
+            //                          console.log("hello from the other side!!!");
+            //            });
 
-	}); 
+            //remove before deploying
+            // console.log("university: "+ results[0].university.name); 
+            // console.log("institute: "+ results[0].institute.name); 
+            // console.log("course: "+ results[0].title.name); 
+            // console.log("result : "+results);
 
-	
+            next(err);
+        });
 
-    
-	// Render the view
-	view.render('tickets/mycard');
-	
+    });
+
+
+
+
+    // Render the view
+    view.render('tickets/mycard');
+
 };
